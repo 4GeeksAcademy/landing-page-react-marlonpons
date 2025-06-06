5f1654e97d612ef59ba3864f63e03b028a819f8c
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-06-06 18:51:52 +0000

    cambios al footer

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
     return (
         <footer className="bg-dark text-white text-center py-3">
             <div className="container">
-                <p className="mb-0">© 2023 Your Company Name. All rights reserved.</p>
+                <p className="mb-0">© 2025 My Company Name. All rights reserved because yes ☺.</p>
                 <ul className="list-inline mt-2">
                     <li className="list-inline-item"><a href="#" className="text-white">Privacy Policy</a></li>
                     <li className="list-inline-item"><a href="#" className="text-white">Terms of Service</a></li>
-                    <li className="list-inline-item"><a href="#" className="text-white">Contact Us</a></li>
+                    <li className="list-inline-item"><a href="#" className="text-white">Contact Me</a></li>
                 </ul>
             </div>
         </footer>
